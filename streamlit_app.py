9433970aeeb7b5e2a87bea7d12e1886b28964c29
jehoppis <56988760+jehoppis@users.noreply.github.com>
2022-12-23 10:29:19 -0600

    add button

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,6 +52,15 @@ try:
 except URLError as e:
   streamlit.error()
 
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+    return my_cur.fetchall()
+
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
 
 streamlit.stop()
 
